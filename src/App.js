473b84d47bf1137ebb484e35c41c9a13c58714e9
agnesimbs <agneswambui504@gmail.com>
2023-05-30 16:40:01 +0300

    Css -bit -done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
+
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-         Weather App
-         <button className="btn btn-primary" >Hello</button>
-      <footer>Coded by agnes</footer></div>
+         <h1>Weather App</h1>
+         <Weather />
+         
+      <footer>
+        Coded by agnes and open-sourced at Github
+        </footer></div>
      
     </div>
   );
